0fe569d6c2a359ff61524c95579c67b71bf0cf67
Gauthier-THP <gauthier.sayarath@gmail.com>
2019-02-05 21:41:18 +0100

    alert

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -11,7 +11,7 @@ class GossipsController < ApplicationController
     if @gossip.save # essaie de sauvegarder en base @gossip
       # si ça marche, il redirige vers la page d'index du site
       redirect_to '/'
-      flash[:sucess] = "Sucess"
+      flash[:success] = "Success"
     else
       # sinon, il render la view new (qui est celle sur laquelle on est déjà)
       flash[:error] = "Error"
