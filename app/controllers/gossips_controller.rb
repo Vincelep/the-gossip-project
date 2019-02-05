867d579f635f01190e2f1d5e5b1e058248a00dec
Gauthier-THP <gauthier.sayarath@gmail.com>
2019-02-05 21:26:55 +0100

    zzz

---
+++ app/controllers/gossips_controller.rb
@@ -0,0 +1,23 @@
+class GossipsController < ApplicationController
+
+  def new
+    @gossip = Gossip.new
+    
+  end
+
+  def create
+    @gossip = Gossip.new(:user => User.find(10), :title => params[:title], :content => params[:content]) # avec xxx qui sont les données obtenues à partir du formulaire
+
+    if @gossip.save # essaie de sauvegarder en base @gossip
+      # si ça marche, il redirige vers la page d'index du site
+      redirect_to '/'
+      flash[:sucess] = "Sucess"
+    else
+      # sinon, il render la view new (qui est celle sur laquelle on est déjà)
+      flash[:error] = "Error"
+      render :new
+    end
+
+  end
+
+end
