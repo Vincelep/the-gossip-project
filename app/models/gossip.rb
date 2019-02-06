75b496da015e3d17bb41520a54dad58d2e15092b
vincelep <vincent.leparoux@gmail.com>
2019-02-06 11:33:47 +0100

    modif avant heroku

--- app/models/gossip.rb
+++ app/models/gossip.rb
@@ -1,9 +1,9 @@
 class Gossip < ApplicationRecord
   validates :title,
     presence: true,
-    length: { in: 3..14 }
+    length: { in: 3..400 }
   validates :content, presence: true
-  
+
   belongs_to :user
   has_many :tags
   #has_many :comments
